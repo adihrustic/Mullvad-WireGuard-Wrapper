0dc9bb91a9bb13de3e734931fb9f8793b2d3bd9b
adihrustic <hrustic@student.chalmers.se>
2019-08-13 13:54:42 +0200

    Added option to choose DNS server. Fixed major and minor bugs. Changed weird logic.
    Signed-off-by: adihrustic <hrustic@student.chalmers.se>

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-TARGET_USER=$SUDO_USER
 TARGET_HOME=$(eval echo ~"$SUDO_USER")
 CONFIG_ROOT="${TARGET_HOME}/.config/wvpn"
 SERVER_LIST="${CONFIG_ROOT}/servers"
-DEFAULT_SERVER="${CONFIG_ROOT}/default_server"
-DEFAULT_PROVIDER="${CONFIG_ROOT}/provider"
+DEFAULTS="${CONFIG_ROOT}/defaults"
 
 check_root() {
     if [[ ! $UID == 0 ]]; then
@@ -20,6 +17,7 @@ settings() {
         read -r ANS
         if [[ ${ANS} =~ ^.+$ ]]; then
            CMD_ROOT=${ANS}
+
         else
            CMD_ROOT="/usr/local/bin/"
         fi
@@ -30,22 +28,21 @@ settings() {
 
     echo -e "\nChoose your provider: "
     PS3="> "
-    options=("Mullvad" "Azire" "Other")
+    options=("Azire" "Mullvad" "Other")
     select opt in "${options[@]}"
     do
         if [[ ${REPLY} == 1 ]]; then
-            PROVIDER="https://mullvad.net/media/files/mullvad-wg.sh"
-        elif [[ ${REPLY} == 2 ]]; then
             PROVIDER="https://www.azirevpn.com/dl/azirevpn-wg.sh"
+
+        elif [[ ${REPLY} == 2 ]]; then
+            PROVIDER="https://mullvad.net/media/files/mullvad-wg.sh"
+
         elif [[ ${REPLY} == 3 ]]; then
             while :; do
                 echo -en "Please enter the full url to your wireguard config file:\n> "
                 read -r ANS
-                if [[ ${ANS} =~ ^.+$ ]]; then
-                    PROVIDER=${ANS}
-                else
-                    continue
-                fi
+                [[ ${ANS} =~ ^.+$ ]] || continue
+                PROVIDER=${ANS}
                 echo -n "Download from ${PROVIDER}? [Y/n] "
                 read -r ANS
                 [[ ${ANS} =~ ^(Y|y|^$)$ ]] && break;
@@ -59,16 +56,44 @@ settings() {
     options=("Both (default)" "IPv4" "IPv6")
     select opt in "${options[@]}"
     do
-        [[ ${REPLY} == 1 ]] && IP=1
-        [[ ${REPLY} == 2 ]] && IP=2
-        [[ ${REPLY} == 3 ]] && IP=3
+        [[ ${REPLY} == 1 ]] && IP="B"
+        [[ ${REPLY} == 2 ]] && IP=4
+        [[ ${REPLY} == 3 ]] && IP=6
         [[ ${IP} ]] && break
+    done
+
+    echo -en "\nSet Cloudflare (1.1.1.1) as DNS? [Y/n/o(ther)] "
+    while :; do
+        read -r ANS
+
+        if [[ $ANS =~ ^(Y|y|^$)$ ]]; then
+            DNS="1.1.1.1"
+
+        elif [[ ${ANS} =~ ^(N|n)$ ]]; then
+            echo "No DNS will be set"
+
+        elif [[ ${ANS} =~ ^o$ ]]; then
+            while :; do
+                echo -en "Please enter a correct DNS IP:> "
+                read -r ANS
+                [[ ${ANS} =~ [0-9]+.[0-9]+.[0-9]+.[0-9]+ ]] || continue
+                DNS=${ANS}
+                echo -n "Set ${DNS} as DNS? [Y/n] "
+                read -r ANS
+                [[ ${ANS} =~ ^(Y|y|^$)$ ]] && break;
+            done
+
+        else
+            echo -n "Invalid input, please try again [Y/n/(o)ther] "
+            continue
+        fi
+        break
     done
 
     echo -en "\nTurn on kill-switch for all servers? [Y/n] "
     while :; do
         read -r ANS
-        [[ $ANS =~ ^(y|Y|^$)$ ]] && KS_YES=true && break
+        [[ $ANS =~ ^(y|Y|^$)$ ]] && KILL_SWITCH=1 && break
         [[ $ANS =~ ^(n|N)$ ]] && break
         echo -n "Invalid input, please try again [Y/n] "
     done
@@ -84,9 +109,9 @@ install() {
     sed -i "${LINE}i${SERVER}" ./wvpn-wg.sh
 
     #Injecting IP version control
-    [[ $IP == 1 ]] && ADDRESS_SETTING='$ADDRESS'         #Both
-    [[ $IP == 2 ]] && ADDRESS_SETTING='${ADDRESS\/,*\/}' #v4
-    [[ $IP == 3 ]] && ADDRESS_SETTING='${ADDRESS\/*,\/}' #v6
+    [[ $IP == "B" ]] && ADDRESS_SETTING='$ADDRESS'        #Both
+    [[ $IP -eq 4 ]] && ADDRESS_SETTING='${ADDRESS\/,*\/}' #v4
+    [[ $IP -eq 6 ]] && ADDRESS_SETTING='${ADDRESS\/*,\/}' #v6
     REGEX="s/\(Address = \).*/\1${ADDRESS_SETTING}/"
     sed -i "$REGEX" ./wvpn-wg.sh
 
@@ -94,40 +119,52 @@ install() {
     REGEX="s/\(CONFIGURATION_FILE=.*wireguard\/\).*\(-\$CODE.conf\)/\1wvpn\2/"
     sed -i "$REGEX" ./wvpn-wg.sh
 
-    chmod +x ./wvpn-wg.sh
     $(bash ./wvpn-wg.sh) &>/dev/null
+    [[ $? -ne 0 ]] || exit 1
 
-    if [[ $KS_YES ]]; then
+    #Injecting DNS
+    if [[ ${DNS} ]]; then
+        for file in /etc/wireguard/wvpn*; do
+            LINE=$(sed -n '/DNS/=' "$file")
+            if [[ $LINE ]]; then
+                sed -i "s/DNS =.*/DNS = ${DNS}/g" "$file"
+            else
+                sed -i "${LINE}i DNS = ${DNS}" "$file"
+            fi
+        done
+    fi
+
+    #Injecting Kill switch
+    if [[ ${KILL_SWITCH} ]]; then
         PostUp="iptables -I OUTPUT ! -o %i -m mark ! --mark \$(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT"
         PostUp=${PostUp/*/"PostUp = $PostUp && ${PostUp//ip/ip6}"}
         PreDown=${PostUp//-I/-D}
         PreDown=${PreDown//PostUp/PreDown}
-        echo -e "\nTurning on kill-switch for servers\n"
+        echo -e "Turning on kill-switch for servers\n"
         for file in /etc/wireguard/wvpn*; do
-            sed -i "4a $PostUp\n$PreDown" "$file"
+            sed -i "/Peer/i$PostUp\n$PreDown" "$file"
         done
     fi
-
+    #Setting defaults
     mkdir -p "${CONFIG_ROOT}"
-    echo "${PROVIDER}" > "${DEFAULT_PROVIDER}"
     sort --version-sort ./servers.tmp > "${SERVER_LIST}"
-    echo
     cat "${SERVER_LIST}"
     echo -e "\nFrom the above list, please select a default server:"
     AVAILABLE_SERVERS=$(awk -F':' '{print $1}' "${SERVER_LIST}")
     PS3="> "
     select opt in $AVAILABLE_SERVERS
     do
-        grep -q "${opt:-"null"}" <<< "${AVAILABLE_SERVERS}" || continue
-        echo "${opt}" > "${DEFAULT_SERVER}"
+        [[ $REPLY ]] || continue
+        echo "DEFAULT_SERVER=${opt}" > "${DEFAULTS}"
         break
     done
+    echo -e "PROVIDER=${PROVIDER}" "\nIP=${IP}" "\nDNS=${DNS}" >> "${DEFAULTS}"
 
-    chown -R "${TARGET_USER}": "${CONFIG_ROOT}"
+    chown -R "${SUDO_USER}": "${CONFIG_ROOT}"
     cp ./wvpn ${CMD_ROOT}
     cp ./completion/wvpn /usr/share/bash-completion/completions/
     rm ./servers.tmp ./wvpn-wg.sh
-    echo "Installed! Please wait up to 60 seconds for your public key to be added to the servers."
+    echo -e "\nInstalled! Please wait up to 60 seconds for your public key to be added to the servers."
     exit 0
 }
 
